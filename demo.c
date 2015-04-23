871c8153edbbff3809a74735969dd5bf05b4fd39
MateuszPaterak <mzt@onet.eu>
2015-04-23 19:37:59 +0200

    Added RNG from STM

--- demo.c
+++ demo.c
@@ -1,17 +1,33 @@
 #include "snakelib.h"
 #include "tm_stm32f4_pcd8544.h"
-#include "tm_stm32f4_rng.h"
+//#include "tm_stm32f4_rng.h"
 #include "main_menu_lib.h"
 #include "demo.h"
 
+#include "stm32f4xx_rng.h"
 
-
+/*
 void DemoRandomPikselBoxGenerator(void)
     {
     uint32_t x,y;
 
     x = TM_RNG_Get() % (84/BOXDIM);
     y = TM_RNG_Get() % (48/BOXDIM);
+
+   // DrawEmptyBoxInGrid(x,y);
+    DrawFilledBoxInGrid(x,y,PCD8544_Pixel_Set);
+    }
+*/
+
+/*
+ * STMLibRNG
+ */
+void DemoRandomPikselBoxGenerator(void)
+    {
+    uint32_t x,y;
+
+    x = RNG_GetRandomNumber() % (84/BOXDIM);
+    y = RNG_GetRandomNumber() % (48/BOXDIM);
 
    // DrawEmptyBoxInGrid(x,y);
     DrawFilledBoxInGrid(x,y,PCD8544_Pixel_Set);
--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include "include.h"
 #include "bitmap.h"
+#include "stm32f4xx_rng.h"
 
 int main(void)
 {
     //180MHz
     SystemInit();
 
-    TM_RNG_Init(); //RNG start
+
+    RCC_AHB2PeriphClockCmd(RCC_AHB2Periph_RNG, ENABLE);
+    RNG_Cmd(ENABLE); //switch on
+    /*
+    RNG_ClearFlag(RNG_FLAG_CECS);
+    while (RNG_GetFlagStatus(RNG_FLAG_DRDY == RESET)) {   }
+    */
 
     //Initialize LCD with 0x38 software contrast
     PCD8544_Init(0x38);
@@ -24,5 +31,5 @@ int main(void)
 	PCD8544_Delay(1000000);
 
 
-    }
+	}
 }
